f52f3fef42bda6cc9596cc7dc8c7ff94e780bc9d
u-masao <4973920+u-masao@users.noreply.github.com>
2023-06-06 00:31:48 +0900

    [update] remove unused

--- src/utils.py
+++ src/utils.py
@@ -2,11 +2,11 @@ import os
 import pickle
 from pathlib import Path
 from typing import Any
-import numpy as np
-import pymc3 as pm
 
 
-def save_trace_and_model(trace:Any, model:Any, model_path_string: str)->None:
+def save_trace_and_model(
+    trace: Any, model: Any, model_path_string: str
+) -> None:
     # save trace and model
     save_path = Path(model_path_string)
     os.makedirs(save_path.parent, exist_ok=True)
