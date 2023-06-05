4f496696869f840f863f3f15fd129c3d86bfba01
u-masao <4973920+u-masao@users.noreply.github.com>
2023-06-05 23:38:12 +0900

    [update] add stab output

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,17 +1,28 @@
+import json
 import logging
-from typing import Any, Optional
+from typing import Any, Dict
 
 import click
 
 
+def make_dataset(kwargs: Dict) -> Dict:
+    return {}
+
+
 @click.command()
 @click.argument("output_filepath", type=click.Path())
-def main(**kwargs: Optional[Any]) -> None:
-    """Runs data processing scripts to turn raw data from (../raw) into
-    cleaned data ready to be analyzed (saved in ../processed).
-    """
+def main(**kwargs: Any) -> None:
+    """メイン処理"""
     logger = logging.getLogger(__name__)
-    logger.info("making final data set from raw data")
+    logger.info("start process")
+
+    dataset = make_dataset(kwargs)
+
+    logger.info(f"json output filepath: {kwargs['output_filepath']}")
+    with open(kwargs["output_filepath"], "w") as fo:
+        json.dump(dataset, fo, indent=4)
+
+    logger.info("complete")
 
 
 if __name__ == "__main__":
